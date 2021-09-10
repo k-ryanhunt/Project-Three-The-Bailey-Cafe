311832628a073d217f7eb7246346d11c22f97c69
K Ryan <hunt.kelsearyan@gmail.com>
2021-09-10 12:33:18 -0600

    Updating styling

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -64,8 +64,7 @@ const Cart = () => {
   if (!state.cartOpen) {
     return (
       <div className="cart-closed" onClick={toggleCart}>
-        <span role="img" aria-label="trash">
-         🏃
+        <span role="img" aria-label="basket">🛍️
         </span>
       </div>
     );
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
-import Logo from "./tbclogo.png";
 
 function Nav() {
 
@@ -42,7 +41,6 @@ function Nav() {
 
   return (
     <header className="flex-row px-1">
-       <img src= {Logo} id='tbc' height='45px' width='45px' alt='logo'/>
       <h1>
         <Link to="/">         
           <span role="img" aria-label="logo"> </span>
